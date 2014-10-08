f16de7a515297201727c534fa3845aa553cc83a1
angelamikulasev <angela.mikulasev@hotmail.com>
2014-10-08 17:01:27 +1100

    first commit db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141007232841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "countries", force: true do |t|
     t.string   "country"
     t.datetime "created_at"
