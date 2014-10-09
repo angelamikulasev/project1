13bad8d3222850ae62f2b7e628e2f823e1298887
angelamikulasev <angela.mikulasev@hotmail.com>
2014-10-09 12:22:22 +1100

    fixing null markings on map

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,12 +2,13 @@ class LocationsController < ApplicationController
   def index
     @entries = Entry.all
     @markers = generate_markers
-    @markers
   end
 
   private
 
   def generate_markers
-    @entries.map { |entry| [entry.name, entry.latitude, entry.longitude] }.to_json.html_safe
+    @entries.select { |entry| entry.latitude.present? || entry.longitude.present? }.map do |entry|
+      [entry.name, entry.latitude, entry.longitude]
+    end.to_json.html_safe
   end
 end
