c03f99e73ef4c3300d6f2eca9baf5931a829889d
angelamikulasev <angela.mikulasev@hotmail.com>
2014-12-04 00:12:47 +1100

    add in edit/update current user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,28 @@
 class UsersController < ApplicationController
-	def index
-		@users = User.all
-	end
 
 	def new
 		@user = User.new
 	end
 
 	def create
-		@user = User.new user_params
-		#if 
-		@user.save
-			session[:user_id] = @user.id
-			redirect_to root_path
-		#else
-			#render: new
-		#end
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path, :flash => {:notice => "Welcome to Travel Trax #{ @user.firstname }"}
+    else
+      render :new
+    end
 	end
 
-	def edit
-	end
+  def edit
+    @user = @current_user
+  end
 
-	def update
-	end
+  def update
+    user = @current_user
+    user.update user_params
+    redirect_to country_path
+  end
 
 	def show
 		@user = User.find params[:id]
